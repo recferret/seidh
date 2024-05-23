73414f886f628c5c23dceb0bbcc7d6b2f2b20438
ferretrec <ferretrec@noreply.gitverse.ru>
2024-05-23 17:39:31 +0300

    auth test

--- server/apps/auth/src/main.ts
+++ server/apps/auth/src/main.ts
@@ -6,15 +6,13 @@ import { Logger } from '@nestjs/common';
 const fs = require("fs");
 
 async function bootstrap() {
-  const httpsOptions = {
-    key: fs.readFileSync("./cert/key.pem"),
-    cert: fs.readFileSync("./cert/cert.pem")
-  };
-  console.log(httpsOptions);
+  // const httpsOptions = {
+  //   key: fs.readFileSync("./cert/key.pem"),
+  //   cert: fs.readFileSync("./cert/cert.pem")
+  // };
+  // console.log(httpsOptions);
 
-  const app = await NestFactory.create(AuthModule, {
-    httpsOptions
-  });
+  const app = await NestFactory.create(AuthModule);//, {httpsOptions});
 
   await app.listen(ServicePort.Auth);
 
