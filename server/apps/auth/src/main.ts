0c10e08e85addeadb28987ccf667d8dacba90638
ferretrec <ferretrec@noreply.gitverse.ru>
2024-05-23 17:22:07 +0300

    auth ssl test

--- server/apps/auth/src/main.ts
+++ server/apps/auth/src/main.ts
@@ -3,8 +3,19 @@ import { AuthModule } from './auth.module';
 import { ServicePort } from '@app/seidh-common';
 import { Logger } from '@nestjs/common';
 
+const fs = require("fs");
+
 async function bootstrap() {
-  const app = await NestFactory.create(AuthModule);
+  const httpsOptions = {
+    key: fs.readFileSync("./cert/key.pem"),
+    cert: fs.readFileSync("./cert/cert.pem")
+  };
+  console.log(httpsOptions);
+
+  const app = await NestFactory.create(AuthModule, {
+    httpsOptions
+  });
+
   await app.listen(ServicePort.Auth);
 
   Logger.log(`Auth listening on port ${ServicePort.Auth}`);
