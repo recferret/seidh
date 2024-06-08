d86f8a21bc6a5e7f3818da0c4f9383b6c1d89f87
ferretrec <ferretrec@noreply.gitverse.ru>
2024-06-08 12:20:51 +0300

    play menu interactive

--- client/src/game/scene/home/PlayContent.hx
+++ client/src/game/scene/home/PlayContent.hx
@@ -1,8 +1,7 @@
 package game.scene.home;
 
-import h2d.BlendMode;
+import motion.Actuate;
 import h2d.filter.Displacement;
-import h2d.filter.Mask;
 import game.scene.base.BasicScene;
 
 class Bunny extends h2d.Object {
@@ -42,15 +41,29 @@ class PlayContent extends BasicHomeContent {
     private final rightBunny:Bunny;
     private final screenShadowDisplacementTile:h2d.Tile;
 
+    // 1 - left, 2 - central, 3 - right
+    private var leftRagnarPosX = 0.0;
+    private var leftRagnarPosY = 0.0;
+    private var centralRagnarPosX = 0.0;
+    private var centralRagnarPosY = 0.0;
+    private var rightRagnarPosX = 0.0;
+    private var rightRagnarPosY = 0.0;
+
+    private var currentRagnar = 2;
+    private final leftRagnar:h2d.Bitmap;
+    private final rightRagnar:h2d.Bitmap;
+    private final centralRagnar:h2d.Bitmap;
+
+    private final ragnarLohTile:h2d.Tile;
+    private final ragnarNormTile:h2d.Tile;
+
     public function new(scene:h2d.Scene) {
 		super(scene);
 
-        final textPlayersOnline = new h2d.Text(hxd.res.DefaultFont.get(), this);
-		textPlayersOnline.setPosition(15, 15);
-		textPlayersOnline.text = 'Play';
-		textPlayersOnline.setScale(2);
-
+        // ------------------------------------
         // Grass
+        // ------------------------------------
+
         final grass1Tile =  hxd.Res.terrain.tile_1.toTile().center();
         final grass2Tile =  hxd.Res.terrain.tile_2.toTile().center();
         final grass3Tile =  hxd.Res.terrain.tile_3.toTile().center();
@@ -86,20 +99,29 @@ class PlayContent extends BasicHomeContent {
         final grass10 = new h2d.Bitmap(grass4Tile, this);
         grass10.setPosition(300, BasicScene.ActualScreenHeight - grass2Tile.height * 2);
 
+        // ------------------------------------
         // Puddle
+        // ------------------------------------
+
         final puddleTile =  hxd.Res.terrain.puddle.toTile();
 
         final puddle1 = new h2d.Bitmap(puddleTile, this);
         puddle1.setPosition(BasicScene.ActualScreenWidth - puddleTile.width * 0.7, 480);
 
+        // ------------------------------------
         // Bunnies
+        // ------------------------------------
+
         leftBunny = new Bunny(this, false);
         leftBunny.setPosition(leftBunny.getWidth() / 2, BasicScene.ActualScreenHeight / 1.55);
 
         rightBunny = new Bunny(this, true);
         rightBunny.setPosition(BasicScene.ActualScreenWidth - rightBunny.getWidth() / 2, BasicScene.ActualScreenHeight / 1.55);
 
+        // ------------------------------------
         // Trees
+        // ------------------------------------
+
         final tree1Tile =  hxd.Res.terrain.tree_1.toTile().center();
         final tree2Tile =  hxd.Res.terrain.tree_2.toTile().center();
 
@@ -112,7 +134,10 @@ class PlayContent extends BasicHomeContent {
         final tree3 = new h2d.Bitmap(tree2Tile, this);
         tree3.setPosition(70, BasicScene.ActualScreenHeight - tree2Tile.height * 0.7);
 
+        // ------------------------------------
         // Fence
+        // ------------------------------------
+
         final fence1Tile =  hxd.Res.terrain.fence_1.toTile();
         final fence2Tile =  hxd.Res.terrain.fence_2.toTile();
 
@@ -121,19 +146,33 @@ class PlayContent extends BasicHomeContent {
         final fence2 = new h2d.Bitmap(fence2Tile, this);
         fence2.setPosition(BasicScene.ActualScreenWidth - fence1Tile.width * 2 + 5, BasicScene.ActualScreenHeight / 1.44 + 16);
 
-
+        // ------------------------------------
         // Ragnars
-        final leftRagnar = new h2d.Bitmap(hxd.Res.ragnar.ragnar_loh.toTile().center(), this);
-        leftRagnar.setPosition(100, BasicScene.ActualScreenHeight / 2.5);
+        // ------------------------------------
 
-        final rightRagnar = new h2d.Bitmap(hxd.Res.ragnar.ragnar_loh.toTile().center(), this);
-        rightRagnar.setPosition(BasicScene.ActualScreenWidth - rightRagnar.tile.width / 1.8, BasicScene.ActualScreenHeight / 2.5);
+        ragnarLohTile = hxd.Res.ragnar.ragnar_loh.toTile().center();
+        ragnarNormTile = hxd.Res.ragnar.ragnar_norm.toTile().center();
 
-        final centralRagnar = new h2d.Bitmap(hxd.Res.ragnar.ragnar_norm.toTile().center(), this);
-        centralRagnar.setPosition(BasicScene.ActualScreenWidth / 2, BasicScene.ActualScreenHeight / 2);
-        centralRagnar.setScale(1.1);
+        leftRagnarPosX = 100;
+        leftRagnarPosY = BasicScene.ActualScreenHeight / 2.5;
+        centralRagnarPosX = BasicScene.ActualScreenWidth / 2;
+        centralRagnarPosY = BasicScene.ActualScreenHeight / 2;
+        rightRagnarPosX = BasicScene.ActualScreenWidth - ragnarLohTile.width / 2 - 20;
+        rightRagnarPosY = BasicScene.ActualScreenHeight / 2.5;
 
+        leftRagnar = new h2d.Bitmap(ragnarLohTile, this);
+        leftRagnar.setPosition(leftRagnarPosX, leftRagnarPosY);
+
+        rightRagnar = new h2d.Bitmap(ragnarLohTile, this);
+        rightRagnar.setPosition(rightRagnarPosX, rightRagnarPosY);
+
+        centralRagnar = new h2d.Bitmap(ragnarNormTile, this);
+        centralRagnar.setPosition(centralRagnarPosX, centralRagnarPosY);
+
+        // ------------------------------------
         // Shadow
+        // ------------------------------------
+
         final screenShadow = new h2d.Bitmap(hxd.Res.ui.home.mm_shadow.toTile(), this);
         final scaleFactor = BasicScene.ActualScreenHeight / 1280;
 
@@ -144,15 +183,152 @@ class PlayContent extends BasicHomeContent {
 		screenShadow.scaleY = scaleFactor;
         screenShadow.setPosition(-((screenShadow.tile.width * (scaleFactor - 1)) / 2), 0);
 
+        // ------------------------------------
         // Buttons
+        // ------------------------------------
+
+        // Play button
+        final playButtonInactiveTile = hxd.Res.ui.home.PLAY_nay.toTile();
+        final playButtonActiveTile = hxd.Res.ui.home.PLAY_yay.toTile();
+
+        final playButton = new h2d.Bitmap(playButtonInactiveTile, this);
+        playButton.setPosition(
+            BasicScene.ActualScreenWidth / 2 - playButtonInactiveTile.width / 2, 
+            BasicScene.ActualScreenHeight / 5 - playButtonInactiveTile.height / 2
+        );
+
+        final playButtonInteractive = new h2d.Interactive(playButtonActiveTile.width, playButtonActiveTile.height, playButton);
+        playButtonInteractive.onPush = function(event : hxd.Event) {
+            playButton.tile = playButtonActiveTile;
+        }
+        playButtonInteractive.onRelease = function(event : hxd.Event) {
+            playButton.tile = playButtonInactiveTile;
+        }
+        playButtonInteractive.onClick = function(event : hxd.Event) {
+            // TODO run the game
+        }
+
+        // Play button
+        final lvlButtonInactiveTile = hxd.Res.ui.home.LVL_nay.toTile();
+        final lvlButtonActiveTile = hxd.Res.ui.home.LVL_yay.toTile();
+        
+        final lvlButton = new h2d.Bitmap(lvlButtonInactiveTile, this);
+        lvlButton.setPosition(
+            BasicScene.ActualScreenWidth / 2 - lvlButtonInactiveTile.width / 2, 
+            BasicScene.ActualScreenHeight * 0.8 - lvlButtonInactiveTile.height / 2
+        );
+        
+        final lvlButtonInteractive = new h2d.Interactive(lvlButtonActiveTile.width, lvlButtonActiveTile.height, lvlButton);
+        lvlButtonInteractive.onPush = function(event : hxd.Event) {
+            lvlButton.tile = lvlButtonActiveTile;
+        }
+        lvlButtonInteractive.onRelease = function(event : hxd.Event) {
+            lvlButton.tile = lvlButtonInactiveTile;
+        }
+        lvlButtonInteractive.onClick = function(event : hxd.Event) {
+            // TODO run the game
+        }
+
+        // Prev button
+        final prevRagnarButton = new h2d.Bitmap(hxd.Res.ui.home.arrow_left.toTile(), this);
+        prevRagnarButton.setPosition(
+            prevRagnarButton.tile.width / 2, 
+            BasicScene.ActualScreenHeight / 2 - prevRagnarButton.tile.height / 2
+        );
+
+        final prevRagnarButtonInteractive = new h2d.Interactive(prevRagnarButton.tile.width, prevRagnarButton.tile.height, prevRagnarButton);
+        prevRagnarButtonInteractive.onPush = function(event : hxd.Event) {
+            prevRagnarButton.setScale(1.2);
+        }
+        prevRagnarButtonInteractive.onRelease = function(event : hxd.Event) {
+            prevRagnarButton.setScale(1);
+        }
+        prevRagnarButtonInteractive.onClick = function(event : hxd.Event) {
+            switchRagner('left');
+        }
+
+        // Next button
+        final nextRagnarButton = new h2d.Bitmap(hxd.Res.ui.home.arrow_right.toTile(), this);
+        nextRagnarButton.setPosition(
+            BasicScene.ActualScreenWidth - nextRagnarButton.tile.width * 1.5, 
+            BasicScene.ActualScreenHeight / 2 - nextRagnarButton.tile.height / 2
+        );
+
+        final nextRagnarButtonInteractive = new h2d.Interactive(nextRagnarButton.tile.width, nextRagnarButton.tile.height, nextRagnarButton);
+        nextRagnarButtonInteractive.onPush = function(event : hxd.Event) {
+            nextRagnarButton.setScale(1.2);
+        }
+        nextRagnarButtonInteractive.onRelease = function(event : hxd.Event) {
+            nextRagnarButton.setScale(1);
+        }
+        nextRagnarButtonInteractive.onClick = function(event : hxd.Event) {
+            switchRagner('right');
+        }
+
+
     }
 
     public function update(dt:Float) {
-        // 
-        // trace('update');
-
         screenShadowDisplacementTile.scrollDiscrete(1 * dt, 7 * dt);
         leftBunny.update(dt);
         rightBunny.update(dt);
+    }
+
+    private function switchRagner(dir:String) {
+        if (dir == 'right') {
+            // Current is left, after one spin
+            if (currentRagnar == 1) {
+                Actuate.tween(leftRagnar, 1, { x: rightRagnarPosX, y: rightRagnarPosY });
+                Actuate.tween(centralRagnar, 1, { x: leftRagnarPosX, y: leftRagnarPosY });
+                Actuate.tween(rightRagnar, 1, { x: centralRagnarPosX, y: centralRagnarPosY });
+                currentRagnar = 3;
+                return;
+            }
+
+            // Current is central, this is also default
+            if (currentRagnar == 2) {
+                Actuate.tween(leftRagnar, 1, { x: centralRagnarPosX, y: centralRagnarPosY });
+                Actuate.tween(centralRagnar, 1, { x: rightRagnarPosX, y: rightRagnarPosY });
+                Actuate.tween(rightRagnar, 1, { x: leftRagnarPosX, y: leftRagnarPosY });
+                currentRagnar = 1;
+                return;
+            }
+
+            // Current is right, third spin
+            if (currentRagnar == 3) {
+                Actuate.tween(leftRagnar, 1, { x: leftRagnarPosX, y: leftRagnarPosY });
+                Actuate.tween(centralRagnar, 1, { x: centralRagnarPosX, y: centralRagnarPosY });
+                Actuate.tween(rightRagnar, 1, { x: rightRagnarPosX, y: rightRagnarPosY });
+                currentRagnar = 2;
+                return;
+            }
+        } else if (dir == 'left') {
+            // Current is left, after one spin
+            if (currentRagnar == 1) {
+                Actuate.tween(leftRagnar, 1, { x: centralRagnarPosX, y: centralRagnarPosY });
+                Actuate.tween(centralRagnar, 1, { x: rightRagnarPosX, y: rightRagnarPosY });
+                Actuate.tween(rightRagnar, 1, { x: leftRagnarPosX, y: leftRagnarPosY });
+                currentRagnar = 3;
+                return;
+            }
+
+            // Current is central, this is also default
+            if (currentRagnar == 2) {
+                Actuate.tween(leftRagnar, 1, { x: rightRagnarPosX, y: rightRagnarPosY });
+                Actuate.tween(centralRagnar, 1, { x: leftRagnarPosX, y: leftRagnarPosY });
+                Actuate.tween(rightRagnar, 1, { x: centralRagnarPosX, y: centralRagnarPosY });
+                currentRagnar = 1;
+                return;
+            }
+
+            // Current is right, third spin
+            if (currentRagnar == 3) {
+                Actuate.tween(leftRagnar, 1, { x: leftRagnarPosX, y: leftRagnarPosY });
+                Actuate.tween(centralRagnar, 1, { x: centralRagnarPosX, y: centralRagnarPosY });
+                Actuate.tween(rightRagnar, 1, { x: rightRagnarPosX, y: rightRagnarPosY });
+                currentRagnar = 2;
+                return;
+            }
+        }
     }
 }
