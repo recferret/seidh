254bf4917141bf3d0315d93bf9bbcadbce7b87c8
osman sonmezturk <osmansonmezturk@gmail.com>
2024-07-24 12:01:21 +0300

    ReferralConfig was added

---
+++ server/libs/seidh-common/src/schemas/schema.referral.ts
@@ -0,0 +1,22 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type ReferralConfigDocument = HydratedDocument<ReferralConfig>;
+
+@Schema()
+export class ReferralConfig {
+  @Prop({ default: 1000 })
+  referrerPremiumRewardTokens: number;
+
+  @Prop({ default: 200 })
+  referrerNoPremiumRewardTokens: number;
+
+  @Prop({ default: 200 })
+  referralPremiumRewardTokens: number;
+
+  @Prop({ default: 50 })
+  referralNoPremiumRewardTokens: number;
+}
+
+export const ReferralConfigSchema =
+  SchemaFactory.createForClass(ReferralConfig);
