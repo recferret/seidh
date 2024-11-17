c9d345a68d8fa660139c72fdd4f9663a77bf417b
recferret <verynimbleferret@gmail.com>
2024-11-17 14:25:51 +0300

    screens better sctructure

---
+++ client/src/game/scene/impl/home/CollectionContent.hx
@@ -0,0 +1,133 @@
+package game.scene.impl.home;
+
+import hxd.res.DefaultFont;
+
+import game.js.NativeWindowJS;
+import game.sound.SoundManager;
+import game.Res.SeidhResource;
+
+class WalletButton extends h2d.Object {
+
+    private final w = 0.0;
+    private final h = 0.0;
+    private final interaction:h2d.Interactive;
+
+    public function new(parent:h2d.Object, boostLabel:String, type:String) {
+        super(parent);
+
+        final scaleX = 1.6;
+        final scaleY = 0.5;
+        final bmp = new h2d.Bitmap(Res.instance.getTileResource(SeidhResource.UI_DIALOG_WINDOW_SMALL), this);
+        bmp.scaleX = scaleX;
+        bmp.scaleY = scaleY;
+        w = bmp.tile.width * scaleX;
+        h = bmp.tile.height * scaleY;
+
+        final font : h2d.Font = DefaultFont.get();
+        final tf = new h2d.Text(font);
+        tf.text = boostLabel;
+        tf.textColor = GameConfig.FontColor;
+        tf.dropShadow = { dx : 0.5, dy : 0.5, color : 0xFF0000, alpha : 0.8 };
+        tf.textAlign = Center;
+        tf.setScale(4);
+        tf.setPosition(0, -35);
+
+        addChild(tf);
+        
+        interaction = new h2d.Interactive(w, h);
+        interaction.onPush = function(event : hxd.Event) {
+            setScale(0.9);
+        }
+        interaction.onRelease = function(event : hxd.Event) {
+            setScale(1);
+        }
+        interaction.onClick = function(event : hxd.Event) {
+            if (type == 'connect_wallet') {
+                NativeWindowJS.trackWalletConnectClick();
+
+                NativeWindowJS.tonConnect(function callback(address:String) {
+                    NativeWindowJS.trackWalletConnected(address);
+                    Player.instance.saveWalletAddress(address);
+                    parent.removeChild(interaction);
+                    parent.removeChild(this);
+                });
+            } else if (type == 'mint_ragnar') {
+                NativeWindowJS.trackMintRagnarClick();
+                NativeWindowJS.tonMintRagnar();
+            } else if (type == 'disconnect_wallet') {
+                // TODO analytics
+                NativeWindowJS.tonDisconnect(function callback() {
+                    trace('Wallet disconnected');
+                });
+            }
+
+            SoundManager.instance.playButton2();
+        }
+        parent.addChild(interaction);
+    }
+
+    public function setPosTop() {
+        setPosition(
+            Main.ActualScreenWidth / 2 ,
+            400
+        );
+        interaction.setPosition(
+            Main.ActualScreenWidth / 2 - w / 2,
+            370 - h / 2
+        );
+    }
+
+    public function setPosBelowTop() {
+        setPosition(
+            Main.ActualScreenWidth / 2 ,
+            570
+        );
+        interaction.setPosition(
+            Main.ActualScreenWidth / 2 - w / 2,
+            570 - h / 2
+        );
+    }
+
+}
+
+class CollectionContent extends BasicHomeContent {
+
+    public function new() {
+		super(true);
+
+        // final buyRagnar = new WalletButton(this, 'Buy Ragnar', 'mint_ragnar');
+
+        // if (Player.instance.getWalletAddress() == null) {
+        //     buyRagnar.setPosBelowTop();
+
+        //     final walletConnect = new WalletButton(this, 'Connect wallet', 'connect_wallet');
+        //     walletConnect.setPosTop();
+        // } else {
+        //     final walletDisconnect = new WalletButton(this, 'Disconnect wallet', 'disconnect_wallet');
+        //     walletDisconnect.setPosTop();
+
+        //     buyRagnar.setPosBelowTop();
+        // }
+
+        final ragnarDudeBitmap = new h2d.Bitmap(Res.instance.getTileResource(SeidhResource.RAGNAR_DUDE), this);
+        ragnarDudeBitmap.setPosition(Main.ActualScreenWidth / 2, 570);
+
+        // final tf = new h2d.Text(DefaultFont.get());
+        // tf.text = "2000 / 2000 NFT Ragnars!";
+        // tf.textColor = GameConfig.FontColor;
+        // tf.dropShadow = { dx : 0.5, dy : 0.5, color : 0xFF0000, alpha : 0.8 };
+        // tf.textAlign = Center;
+        // tf.setScale(3);
+        // tf.setPosition(Main.ActualScreenWidth / 2, 670);
+        // addChild(tf);
+
+        final soonTile = Res.instance.getTileResource(SeidhResource.UI_HOME_TITLE_SOON);
+        final soonBitmap = new h2d.Bitmap(this);
+		soonBitmap.setPosition(Main.ActualScreenWidth / 2, 800);
+		soonBitmap.tile = soonTile;
+    }
+
+    public function update(dt:Float) {
+    }
+
+}
---
+++ client/src/game/scene/impl/home/FriendsContent.hx
@@ -0,0 +1,195 @@
+package game.scene.impl.home;
+
+import game.js.NativeWindowJS;
+import game.Res.SeidhResource;
+import game.sound.SoundManager;
+import game.event.EventManager;
+import hxd.res.DefaultFont;
+
+enum abstract FriendStatus(Int) {
+	var OFFLINE = 1;
+	var ONLINE = 2;
+    var PLAYING_POSSIBLE_TO_JOIN = 3;
+    var PLAYING_NOT_POSSIBLE_TO_JOIN = 4;
+}
+
+class Friend extends h2d.Object {
+
+    private var height = 100;
+
+    public function new(parent:h2d.Object, friendStatus:FriendStatus, friendNameText:String, friendRewardText:String) {
+        super(parent);
+
+        var statusText = '';
+        switch (friendStatus) {
+            case OFFLINE:
+                statusText = ', offline';
+            case ONLINE:
+                statusText = ', online';
+            case PLAYING_NOT_POSSIBLE_TO_JOIN:
+                statusText = ', playing';
+            case PLAYING_POSSIBLE_TO_JOIN:
+                statusText = ', playing';
+                // height = 200;
+        }
+
+        final font : h2d.Font = DefaultFont.get();
+
+        final friendName = new h2d.Text(font);
+        friendName.text = friendNameText + statusText;
+        friendName.textColor = GameConfig.FontColor;
+        friendName.dropShadow = { dx : 0.5, dy : 0.5, color : 0xFF0000, alpha : 0.8 };
+        friendName.textAlign = Left;
+        friendName.setScale(4);
+        friendName.setPosition(65, 0);
+        addChild(friendName);
+
+        final friendReward = new h2d.Text(font);
+        friendReward.text = "+" + friendRewardText + " SDH";
+        friendReward.textColor = GameConfig.FontColor;
+        friendReward.dropShadow = { dx : 0.5, dy : 0.5, color : 0xFF0000, alpha : 0.8 };
+        friendReward.textAlign = Right;
+        friendReward.setScale(4);
+        friendReward.setPosition(Main.ActualScreenWidth - friendReward.textWidth - 15, 0);
+        addChild(friendReward);
+    }
+
+    public function getHeight() {
+        return height;
+    }
+
+}
+
+class ScrollingFriendsContainer extends h2d.Object {
+
+    private var friendsTotal = 0;
+    private var friendNextPositionY = 0;
+
+    public function new(parent:h2d.Object) {
+        super(parent);
+    }
+
+    public function addFriend(friendNameText:String, friendStatus:FriendStatus, friendRewardText:String) {
+        final newFriend = new Friend(this, friendStatus, friendNameText, friendRewardText);
+        addChild(newFriend);
+        newFriend.setPosition(0, friendNextPositionY);
+        friendNextPositionY  += newFriend.getHeight();
+        // friendsTotal++;
+    }
+
+}
+
+class InviteFriendButton extends h2d.Object {
+
+    private final w = 0.0;
+    private final h = 0.0;
+    private final bmp:h2d.Bitmap;
+
+    public function new(parent:h2d.Object) {
+        super(parent);
+
+        bmp = new h2d.Bitmap(Res.instance.getTileResource(SeidhResource.UI_HOME_LVL_YAY), this);
+
+        w = bmp.tile.width;
+        h = bmp.tile.height;
+
+        // final font : h2d.Font = DefaultFont.get();
+        // final tf = new h2d.Text(font);
+        // tf.text = 'Invite Friend';
+        // tf.textColor = GameConfig.FontColor;
+        // tf.dropShadow = { dx : 0.5, dy : 0.5, color : 0xFF0000, alpha : 0.8 };
+        // tf.textAlign = Center;
+        // tf.setScale(4);
+        // tf.setPosition(0, -35);
+        // addChild(tf);
+    }
+
+    public function getBitmap() {
+        return bmp;
+    }
+
+    public function getHeight() {
+        return h;
+    }
+
+    public function getWidth() {
+        return w;
+    }
+
+}
+
+class FriendsContent extends BasicHomeContent {
+
+    public function new() {
+		super(true);
+
+        final font : h2d.Font = DefaultFont.get();
+
+        final friendsInvited = new h2d.Text(font);
+        friendsInvited.text = "Friends invited: 10";
+        friendsInvited.textColor = GameConfig.FontColor;
+        friendsInvited.dropShadow = { dx : 0.5, dy : 0.5, color : 0xFF0000, alpha : 0.8 };
+        friendsInvited.textAlign = Left;
+        friendsInvited.setScale(2.5);
+        friendsInvited.setPosition(125, 450);
+        addChild(friendsInvited);
+
+        final friendsOnline = new h2d.Text(font);
+        friendsOnline.text = "Friends online: 2";
+        friendsOnline.textColor = GameConfig.FontColor;
+        friendsOnline.dropShadow = { dx : 0.5, dy : 0.5, color : 0xFF0000, alpha : 0.8 };
+        friendsOnline.textAlign = Left;
+        friendsOnline.setScale(2.5);
+        friendsOnline.setPosition(125, 490);
+        addChild(friendsOnline);
+
+        final totalRewards = new h2d.Text(font);
+        totalRewards.text = "Total earned: +2000 SDH";
+        totalRewards.textColor = GameConfig.FontColor;
+        totalRewards.dropShadow = { dx : 0.5, dy : 0.5, color : 0xFF0000, alpha : 0.8 };
+        totalRewards.textAlign = Left;
+        totalRewards.setScale(2.5);
+        totalRewards.setPosition(125, 530);
+        addChild(totalRewards);
+
+        // Invite button
+        final inviteButton = new InviteFriendButton(this);
+        inviteButton.setPosition(
+            Main.ActualScreenWidth / 2,
+            350
+        );
+
+        final interactionInvite = new h2d.Interactive(inviteButton.getWidth(), inviteButton.getHeight());
+        interactionInvite.setPosition(
+            Main.ActualScreenWidth / 2 - inviteButton.getWidth() / 2,
+            350 - inviteButton.getHeight() / 2
+        );
+        interactionInvite.onPush = function(event : hxd.Event) {
+            inviteButton.setScale(0.9);
+        }
+        interactionInvite.onRelease = function(event : hxd.Event) {
+            inviteButton.setScale(1);
+        }
+        interactionInvite.onClick = function(event : hxd.Event) {
+            SoundManager.instance.playButton2();
+            NativeWindowJS.trackInviteFriendClick();
+            EventManager.instance.notify(EventManager.EVENT_REF_SHARE, {});
+        }
+        addChild(interactionInvite);
+
+        // Friend scrolling container
+        final friendsContainer = new ScrollingFriendsContainer(this);
+        friendsContainer.setPosition(0, 600);
+
+        friendsContainer.addFriend("Sofia", FriendStatus.PLAYING_POSSIBLE_TO_JOIN, "200");
+        friendsContainer.addFriend("Sofia", FriendStatus.PLAYING_NOT_POSSIBLE_TO_JOIN, "200");
+        friendsContainer.addFriend("Sofia", FriendStatus.ONLINE, "200");
+        friendsContainer.addFriend("Sofia", FriendStatus.OFFLINE, "200");
+        friendsContainer.addFriend("Sofia", FriendStatus.OFFLINE, "200");
+        friendsContainer.addFriend("Sofia", FriendStatus.OFFLINE, "200");
+    }
+
+    public function update(dt:Float) {
+    }
+  
+}
